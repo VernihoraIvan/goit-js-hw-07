fde7ccf0d496e159f8539d8d6b4f61b79d0acfc0
VernihoraIvan <vernihorai@gmail.com>
2023-03-26 18:51:11 +0200

    second task completed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+let markup = ``;
+const list = document.querySelector(".gallery");
+galleryItems.forEach(({ original, preview, description }) => {
+  const liItem = document.createElement("li");
+  console.log(liItem);
+  liItem.innerHTML = `<a class="gallery__link" href="${original}">
+  <img 
+  class = "gallery__image"
+  src="${preview}" 
+  alt ="${description}" 
+  >`;
 
-console.log(galleryItems);
+  // ul.append(liItem);
+  return (markup += `<li class = "gallery__item">${liItem.innerHTML}</li>`);
+});
+const splittedMarkup = markup.split(",");
+
+list.innerHTML = splittedMarkup;
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
